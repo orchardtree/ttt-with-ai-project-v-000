6185102e5f6d9521b20eb2ce9bad7744cb39bd89
Jesse Wenger <wengerj@gmail.com>
2021-03-16 21:11:13 +0000

    Automatically backed up by Learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -4,8 +4,8 @@ module Players
     def move(board)
       puts "Select a number 1-9."
       pos = gets.chomp
-      #@@board_pos.delete(pos)
-      #pos
+      @@board_pos.delete(pos)
+      pos
     end
   end
 end
